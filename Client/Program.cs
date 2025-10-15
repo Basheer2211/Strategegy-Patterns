99817db76a03c6b350fdb2ec3f5fb33d37232603
unknown <basheirdaoud@gmail.com>
2025-10-16 00:05:23 +0300

    Added Main method and fixed behaviors

---
+++ Client/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Strategegy_Patterns;
+
+Duck mallard = new MallardDuck(new FlyWithWings(), new Quack());
+mallard.Display();
+mallard.performfly();
+mallard.performQuack();
+
+// نغيّر السلوك أثناء التشغيل:
+mallard.setFly(new FlyWithNoway());
+mallard.performfly();
